856767923eaacab884db78cabceb04ba097d4326
Aalok Shah <aalok@shah.ws>
2011-04-23 22:52:58 -0400

    Update calculator so the members do all the work

--- tutorials/golden/4_calculator/student_calculator.js
+++ tutorials/golden/4_calculator/student_calculator.js
@@ -1,4 +1,42 @@
 
+var OPERATION_NONE = 0;
+var OPERATION_ADD = 1;
+var OPERATION_SUBTRACT = 2;
+var OPERATION_MULTIPLY = 3;
+var OPERATION_DIVIDE = 4;
+
+var currentValue = 0;
+var currentOperation = OPERATION_NONE;
+var memoryValue = 0;
+var clearOnNextEntry = true;
+
+
+var calculatorFunctions = {
+  "MC" : memoryClear,
+  "M+" : memoryAdd,
+  "M-" : memorySubtract,
+  "MR" : memoryRecall,
+  "C" : clear,
+  "+": add,
+  "-" : subtract,
+  "*" : multiply,
+  "/" : divide,
+  "+/-" : negate,
+  "0" : numberPressed,
+  "1" : numberPressed,
+  "2" : numberPressed,
+  "3" : numberPressed,
+  "4" : numberPressed,
+  "5" : numberPressed,
+  "6" : numberPressed,
+  "7" : numberPressed,
+  "8" : numberPressed,
+  "9" : numberPressed,
+  "0" : numberPressed,
+  "." : decimalPointPressed,
+  "=" : equalsPressed
+}
+
 function handleCalculatorButtonPressed(evt) {
   var elem = evt.target;
   var text = getInnerText(elem);
@@ -10,6 +48,18 @@ function handleCalculatorButtonPressed(evt) {
   }
   return false;
 }
+
+function attachButtonEvents() {
+  var elems = document.getElementsByClassName("button");
+  for (var i=0; i<elems.length; i++) {
+    var elem = elems[i];
+    if(getInnerText(elem)) {
+      addEvent(elem, "click", handleCalculatorButtonPressed)
+    }
+  }
+}
+
+addEvent(window, "load", attachButtonEvents);
 
 function updateCurrentValueFromOperation() {
   switch(currentOperation) {
@@ -40,10 +90,14 @@ function memoryClear() {
 
 function memoryAdd() {
   memoryValue += getDisplayAsFloat();
+  setDisplayAsFloat(memoryValue);
+  clearOnNextEntry = true;
 }
 
 function memorySubtract() {
   memoryValue -= getDisplayAsFloat();
+  setDisplayAsFloat(memoryValue);
+  clearOnNextEntry = true;
 }
 
 function memoryRecall() {
